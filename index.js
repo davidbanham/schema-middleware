3c94fd38b4c784a639ba6ae846f1431c4c1e4d64
David Banham <david@banham.id.au>
2016-12-16 19:07:54 +1100

    Fix API

--- index.js
+++ index.js
@@ -2,21 +2,7 @@ const tv4 = require('tv4');
 
 const SCHEMA_MISSING = new Error('Route has no schema');
 
-exports.middleware = (routeSchemas) => {
-  return (req, res, next) => {
-    if (!routeSchemas[req.method]) return next(SCHEMA_MISSING);
-
-    const path = req.baseUrl + req.route.path;
-
-    const target = routeSchemas[req.method][path];
-
-    if (!target) return next(SCHEMA_MISSING);
-
-    return exports.single(target)(req, res, next);
-  };
-};
-
-exports.single = (schema) => (req, res, next) =>
+exports.createMiddleware = (schema) => (req, res, next) =>
     tv4.validate(req.body, schema) ? next() : next(tv4.error);
 
 exports.markValidated = (req, res, next) => {
@@ -29,7 +15,7 @@ exports.mount = (app, routeSchemas, options) => {
     const v = routeSchemas[method];
     Object.keys(v).forEach(route => {
       const schema = v[route];
-      const mw = exports.single(schema);
+      const mw = exports.createMiddleware(schema);
       app[method.toLowerCase()](route, mw);
       if (options && options.markValidated) {
         app[method.toLowerCase()](route, exports.markValidated);
--- test/test.js
+++ test/test.js
@@ -6,13 +6,6 @@ const express = require('express');
 const doubleagent = require('doubleagent');
 const bodyParser = require('body-parser');
 
-const app = express();
-// FIXME this should be reloaded in a beforeEach to keep tests separate
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const doubleApp = doubleagent(app);
 
 const valid = {
   id: '3640c173-5821-4112-b61c-77c8091c8346',
@@ -40,97 +33,76 @@ describe('middleware', () => {
       routeSchemas[link.method][link.expressRoute || link.href] = link.schema;
     };
 
-    middleware = Middleware.middleware(routeSchemas);
+    middleware = Middleware.createMiddleware(routeSchemas.POST['/users']);
   });
 
   it('should validate a valid body', (done) => {
     middleware({
       body: valid,
-      path: '/users',
-      method: 'POST',
-      baseUrl: '/users',
-      route: {
-        path: ''
-      }
     }, null, done);
   });
 
   it('should reject an invalid body', (done) => {
     middleware({
       body: invalid,
-      path: '/users',
-      method: 'POST',
-      baseUrl: '/users',
-      route: {
-        path: ''
-      }
     }, null, err => {
       assert(err.message === 'Missing required property: name');
       done();
     });
   });
 
-  it('should fail gracefully on an unschemaed route', (done) => {
-    middleware({
-      body: valid,
-      path: '/nope',
-      method: 'POST',
-      baseUrl: '/nope',
-      route: {
-        path: ''
-      }
-    }, null, err => {
-      assert(err.message === 'Route has no schema');
-      done();
-    });
-  });
-
-  it('should handle interpolated routes', (done) => {
-    middleware({
-      body: valid,
-      path: '/users/3640c173-5821-4112-b61c-77c8091c8346',
-      method: 'PUT',
-      baseUrl: '/users/',
-      route: {
-        path: ':id'
-      }
-    }, null, done);
-  });
 
   describe('mount', () => {
-    before(() => {
-      Middleware.mount(app, routeSchemas);
+    let app;
+    let doubleApp;
+
+    beforeEach(() => {
+      app = express();
+
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+
+      doubleApp = doubleagent(app);
+
     });
-
-    it('should ignore GETs', () => {
-      return doubleApp.get('/users');
-    });
-
-    it('should validate a valid body', () => {
-      return doubleApp.post('/users', valid);
-    });
-
-    it('should reject an invalid body', async () => {
-      let thrown = false;
-
-      const res = await doubleApp.post('/users', invalid);
-
-      assert(res.status === 500);
-      assert(res.text.indexOf('ValidationError') > -1);
-    });
-  });
-
-  describe('markValidated', () => {
-    before(() => {
-      Middleware.mount(app, routeSchemas, {markValidated: true});
-    });
-
-    it('should mark req a validated if asked', async () => {
-      app.post('/users', (req, res, next) => {
-        assert(req.schemaValidated);
-        res.send('ok');
+    describe('no options', () => {
+      beforeEach(() => {
+        Middleware.mount(app, routeSchemas);
       });
-      await doubleApp.post('/users', valid);
+
+      it('should ignore GETs', () => {
+        return doubleApp.get('/users');
+      });
+
+      it('should validate a valid body', () => {
+        return doubleApp.post('/users', valid);
+      });
+
+      it('should reject an invalid body', async () => {
+        let thrown = false;
+
+        const res = await doubleApp.post('/users', invalid);
+
+        assert(res.status === 500);
+        assert(res.text.indexOf('ValidationError') > -1);
+      });
+    });
+
+    describe('markValidated', () => {
+      beforeEach(() => {
+        Middleware.mount(app, routeSchemas, {markValidated: true});
+      });
+
+      it('should mark req a validated if asked', async () => {
+        let marked = false;
+        app.post('/users', (req, res, next) => {
+          console.log('schemaValidated is', req.schemaValidated);
+          marked = req.schemaValidated;
+          res.send('ok');
+        });
+        await doubleApp.post('/users', valid);
+        assert(marked);
+      });
     });
   });
 });
